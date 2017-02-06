6845d3b17e09f0ee8d23a611ad96129d03c62ecc
Nikolay Marusov <nmarusov@gmail.com>
2017-02-06 16:55:10 +0300

    Console scripts patched according to jshint.

--- app/scripts/directives/console.js
+++ app/scripts/directives/console.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
 
-angular.module("homeuiApp")
-  .value("scrollTimeoutMs", 100)
+angular.module('homeuiApp')
+  .value('scrollTimeoutMs', 100)
 
-  .directive("console", function (mqttClient, $rootScope, $timeout,
+  .directive('console', function (mqttClient, $rootScope, $timeout,
                                   dateFilter, getTime, scrollTimeoutMs) {
     mqttClient.addStickySubscription(
-      "/wbrules/log/+", function (options) {
+      '/wbrules/log/+', function (options) {
         $rootScope.$broadcast(
-          "wbrulesLog",
-          options.topic.replace(/^.*\//, ""),
+          'wbrulesLog',
+          options.topic.replace(/^.*\//, ''),
           options.payload);
       });
 
@@ -18,48 +18,50 @@ angular.module("homeuiApp")
       localEnabled: false
     };
 
-    $rootScope.$watch("ruleDebug.enabled", function (newValue) {
+    $rootScope.$watch('ruleDebug.enabled', function (newValue) {
       if (!!newValue !== $rootScope.ruleDebug.localEnabled) {
         mqttClient.send(
-          "/devices/wbrules/controls/Rule debugging/on",
-          newValue ? "1" : "0", false, 1);
+          '/devices/wbrules/controls/Rule debugging/on',
+          newValue ? '1' : '0', false, 1);
         $rootScope.ruleDebug.localEnabled = !!newValue;
       }
     });
 
     mqttClient.addStickySubscription(
-      "/devices/wbrules/controls/Rule debugging", function (options) {
-        $rootScope.ruleDebug.enabled = $rootScope.ruleDebug.localEnabled = options.payload == "1";
+      '/devices/wbrules/controls/Rule debugging', function (options) {
+        $rootScope.ruleDebug.enabled = $rootScope.ruleDebug.localEnabled = (options.payload === '1');
       });
 
     return {
-      restrict: "EA",
+      restrict: 'EA',
       replace: true,
       scope: true,
-      templateUrl: "scripts/directives/console.html",
-      link: function (scope, element, attrs) {
+      templateUrl: 'scripts/directives/console.html',
+      link: function (scope, element) {
         var scrollTimeout = null,
-            messageContainer = element.find(".console-messages");
-        scope.$on("wbrulesLog", function (event, level, message) {
-          var msgEl = $("<div class='console-message'></div>")
-                .addClass("console-message-level-" + level),
+            messageContainer = element.find('.console-messages');
+        scope.$on('wbrulesLog', function (event, level, message) {
+          var msgEl = $('<div class="console-message"></div>')
+                .addClass('console-message-level-' + level),
               ts = getTime();
-          $("<span class='console-message-ts'></span>")
-            .text(dateFilter(ts, "yyyy-MM-dd HH:mm:ss"))
+          $('<span class="console-message-ts"></span>')
+            .text(dateFilter(ts, 'yyyy-MM-dd HH:mm:ss'))
             .appendTo(msgEl);
-          var textEl = $("<span class='console-message-text'></span>")
+          var textEl = $('<span class="console-message-text"></span>')
             .text(message)
             .appendTo(msgEl);
-          textEl.html(textEl.html().replace(/\n/g, "<br>"));
+          textEl.html(textEl.html().replace(/\n/g, '<br>'));
           msgEl.appendTo(messageContainer);
-          if (scrollTimeout !== null)
+          if (scrollTimeout !== null) {
             $timeout.cancel(scrollTimeout);
+          }
           scrollTimeout = $timeout(function () {
             scrollTimeout = null;
-            var scrollHeight = messageContainer.prop("scrollHeight"),
+            var scrollHeight = messageContainer.prop('scrollHeight'),
                 elementHeight = messageContainer.height();
-            if (elementHeight < scrollHeight)
+            if (elementHeight < scrollHeight) {
               messageContainer.scrollTop(scrollHeight);
+            }
           }, scrollTimeoutMs);
         });
       }
--- test/spec/directives/console.js
+++ test/spec/directives/console.js
@@ -1,39 +1,39 @@
-"use strict";
+'use strict';
 
-describe("Directive: console", function () {
+describe('Directive: console', function () {
   var f, FakeTime, scope, container, element;
 
-  beforeEach(module("homeuiApp"));
-  beforeEach(module("homeuiApp.fakeTime"));
-  beforeEach(module("homeuiApp.fakeMqtt"));
+  beforeEach(module('homeuiApp'));
+  beforeEach(module('homeuiApp.fakeTime'));
+  beforeEach(module('homeuiApp.fakeMqtt'));
 
   beforeEach(inject(function ($rootScope, _FakeTime_, _FakeMqttFixture_, $compile, $templateCache) {
     // FIXME: workaround for abolute path in template cache
-    $templateCache.put("scripts/directives/console.html",
-                       $templateCache.get("/scripts/directives/console.html"));
+    $templateCache.put('scripts/directives/console.html',
+                       $templateCache.get('/scripts/directives/console.html'));
     FakeTime = _FakeTime_;
     // Note that month index '5' means June here due to JS Date() API specifics
     FakeTime.setTime(new Date(2015, 5, 19, 20, 25, 6));
     f = _FakeMqttFixture_;
     f.useJSON = false;
     scope = f.$rootScope.$new();
-    container = $("<div></div>")
-      .appendTo($("body"));
-    element = $compile("<console></console>")(scope, function (clonedElement) {
+    container = $('<div></div>')
+      .appendTo($('body'));
+    element = $compile('<console></console>')(scope, function (clonedElement) {
       container.append(clonedElement);
     });
     f.connect();
     f.$rootScope.$digest();
-    element.find(".console-messages").css({
-      fontSize: "12px",
-      position: "absolute",
+    element.find('.console-messages').css({
+      fontSize: '12px',
+      position: 'absolute',
       left: 0,
       top: 0,
-      width: "200px",
-      maxHeight: "50px",
+      width: '200px',
+      maxHeight: '50px',
       margin: 0,
       padding: 0,
-      overflowY: "auto"
+      overflowY: 'auto'
     });
   }));
 
@@ -42,95 +42,96 @@ describe("Directive: console", function () {
   });
 
   function extractMessages () {
-    return element.find(".console-message").toArray().map(function (el) {
+    return element.find('.console-message').toArray().map(function (el) {
       el = $(el);
-      var levelClasses = el.prop("className")
-            .replace(/^\s+|\s+$/g, "")
+      var levelClasses = el.prop('className')
+            .replace(/^\s+|\s+$/g, '')
             .split(/\s+/)
             .filter(function (className) {
               return /^console-message-level-/.test(className);
             }),
-          ts = el.find(".console-message-ts"),
-          text = el.find(".console-message-text");
+          ts = el.find('.console-message-ts'),
+          text = el.find('.console-message-text');
       expect(levelClasses.length).toBe(1);
       expect(ts).toHaveLength(1);
       expect(text).toHaveLength(1);
       return {
-        level: levelClasses[0].replace(/^console-message-level-/, ""),
+        level: levelClasses[0].replace(/^console-message-level-/, ''),
         ts: ts.text(),
         text: text.text()
       };
     });
   }
 
-  it("should not display any messages initially", function () {
+  it('should not display any messages initially', function () {
     expect(extractMessages()).toEqual([]);
   });
 
-  it("should receive and display console messages", function () {
-    f.extClient.send("/wbrules/log/info", "Info message");
+  it('should receive and display console messages', function () {
+    f.extClient.send('/wbrules/log/info', 'Info message');
     expect(extractMessages()).toEqual([
-      { level: "info", ts: "2015-06-19 20:25:06", text: "Info message" }
+      { level: 'info', ts: '2015-06-19 20:25:06', text: 'Info message' }
     ]);
 
     FakeTime.setTime(new Date(2015, 5, 19, 20, 25, 16));
-    f.extClient.send("/wbrules/log/debug", "Debug message");
+    f.extClient.send('/wbrules/log/debug', 'Debug message');
     FakeTime.setTime(new Date(2015, 5, 19, 20, 25, 26));
-    f.extClient.send("/wbrules/log/warning", "Warning message");
+    f.extClient.send('/wbrules/log/warning', 'Warning message');
     FakeTime.setTime(new Date(2015, 5, 19, 20, 25, 36));
-    f.extClient.send("/wbrules/log/error", "Error message");
+    f.extClient.send('/wbrules/log/error', 'Error message');
 
     expect(extractMessages()).toEqual([
-      { level: "info",    ts: "2015-06-19 20:25:06", text: "Info message" },
-      { level: "debug",   ts: "2015-06-19 20:25:16", text: "Debug message" },
-      { level: "warning", ts: "2015-06-19 20:25:26", text: "Warning message" },
-      { level: "error",   ts: "2015-06-19 20:25:36", text: "Error message" }
+      { level: 'info',    ts: '2015-06-19 20:25:06', text: 'Info message' },
+      { level: 'debug',   ts: '2015-06-19 20:25:16', text: 'Debug message' },
+      { level: 'warning', ts: '2015-06-19 20:25:26', text: 'Warning message' },
+      { level: 'error',   ts: '2015-06-19 20:25:36', text: 'Error message' }
     ]);
   });
 
-  it("should scroll to the bottom after receiving messages", function () {
+  it('should scroll to the bottom after receiving messages', function () {
     for (var i = 0; i < 60; ++i) {
       FakeTime.setTime(new Date(2015, 5, 19, 20, 25, 16));
-      f.extClient.send("/wbrules/log/info", "Info message");
-      if (i > 30)
+      f.extClient.send('/wbrules/log/info', 'Info message');
+      if (i > 30) {
         f.$timeout.flush();
+      }
     }
-    var messagesEl = element.find(".console-messages");
+    var messagesEl = element.find('.console-messages');
     expect(messagesEl).toExist();
     expect(messagesEl.scrollTop()).toBeGreaterThan(0);
-    expect(messagesEl.scrollTop()).toBe(messagesEl.prop("scrollHeight") - messagesEl.height());
+    expect(messagesEl.scrollTop()).toBe(messagesEl.prop('scrollHeight') - messagesEl.height());
   });
 
-  describe("toggle switch", function () {
+  describe('toggle switch', function () {
     var sw;
 
     beforeEach(function () {
-      sw = container.find("input[type=checkbox][name='debug']");
-      f.extClient.subscribe("/devices/wbrules/controls/Rule debugging/on", f.msgLogger("ext"));
+      sw = container.find('input[type=checkbox][name="debug"]');
+      f.extClient.subscribe('/devices/wbrules/controls/Rule debugging/on', f.msgLogger('ext'));
     });
 
-    it("should accept 'Rule debugging' values", function () {
+    it('should accept "Rule debugging" values', function () {
       expect(sw).toExist();
-      expect(sw.prop("checked")).toBe(false);
-      f.extClient.send("/devices/wbrules/controls/Rule debugging", "1");
+      expect(sw.prop('checked')).toBe(false);
+      f.extClient.send('/devices/wbrules/controls/Rule debugging', '1');
       f.$rootScope.$digest();
-      expect(sw.prop("checked")).toBe(true);
-      f.extClient.send("/devices/wbrules/controls/Rule debugging", "0");
+      expect(sw.prop('checked')).toBe(true);
+      f.extClient.send('/devices/wbrules/controls/Rule debugging', '0');
       f.$rootScope.$digest();
-      expect(sw.prop("checked")).toBe(false);
+      expect(sw.prop('checked')).toBe(false);
     });
 
-    it("should toggle 'Rule debugging' when clicked", function () {
+    it('should toggle "Rule debugging" when clicked', function () {
       f.expectJournal().toEqual([]);
       sw.click();
       f.$rootScope.$digest();
       f.expectJournal().toEqual([
-        "ext: /devices/wbrules/controls/Rule debugging/on: [1] (QoS 1)"
+        'ext: /devices/wbrules/controls/Rule debugging/on: [1] (QoS 1)'
       ]);
       sw.click();
       f.$rootScope.$digest();
       f.expectJournal().toEqual([
-        "ext: /devices/wbrules/controls/Rule debugging/on: [0] (QoS 1)"
+        'ext: /devices/wbrules/controls/Rule debugging/on: [0] (QoS 1)'
       ]);
     });
   });
